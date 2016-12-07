b543b677d5af1660d4918aae8753eccd716a4391
Boris Gordon <boris@boztek.net>
2016-12-08 07:27:55 +1100

    Mirage should match actual payload so that adapter/serializer is used for tests etc.

--- mirage/config.js
+++ mirage/config.js
@@ -3,45 +3,134 @@ export default function() {
 
   let rentals = [
     {
-      type: 'rentals',
-      id: '17c45e2d-403a-4ef0-8137-045d30d24111',
+      type: 'node--rental',
+      id: '1234',
       attributes: {
         title: 'Grand Old Mansion',
         field_owner: 'Veruca Salt',
         field_city: 'San Francisco',
-        type: 'Estate',
         field_bedrooms: 15,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
         field_description: "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.",
         slug: 'grand-old-mansion'
+      },
+      relationships: {
+        field_image: {
+          data: {
+            type: 'file--file',
+            id: '42'
+          }
+        },
+        field_type: {
+          data: {
+            type: 'taxonomy_term--rental_type',
+            id: '100'
+          }          
+        }
       }
     }, {
-      type: 'rentals',
-      id: '17c45e2d-403a-4ef0-8137-045d30d24112',
+      type: 'node--rental',
+      id: '5678',
       attributes: {
         title: 'Urban Living',
         field_owner: 'Mike Teavee',
         field_city: 'Seattle',
         type: 'Condo',
         field_bedrooms: 1,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
         field_description: "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.",
         slug: 'urban-living'
+      },
+      relationships: {
+        field_image: {
+          data: {
+            type: 'file--file',
+            id: '43'
+          }
+        },
+        field_type: {
+          data: {
+            type: 'taxonomy_term--rental_type',
+            id: '101'
+          }          
+        }
       }
     }, {
-      type: 'rentals',
-      id: '17c45e2d-403a-4ef0-8137-045d30d24113',
+      type: 'node--rental',
+      id: '9012',
       attributes: {
         title: 'Downtown Charm',
         field_owner: 'Violet Beauregarde',
         field_city: 'Portland',
         type: 'Apartment',
         field_bedrooms: 3,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
         field_description: "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet.",
         slug: 'downtown-charm'
+      },
+      relationships: {
+        field_image: {
+          data: {
+            type: 'file--file',
+            id: '44'
+          }
+        },
+        field_type: {
+          data: {
+            type: 'taxonomy_term--rental_type',
+            id: '102'
+          }          
+        }
       }
   }];
+
+  let files = [
+    {
+      type: 'file--file',
+      id: '42',
+      attributes: {
+        filename: 'grand-old-mansion',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+      }
+    },
+    {
+      type: 'file--file',
+      id: '43',
+      attributes: {
+        filename: 'grand-old-mansion',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+      }
+    },
+    {
+      type: 'file--file',
+      id: '44',
+      attributes: {
+        filename: 'grand-old-mansion',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+      }
+    }
+  ];
+
+  let types = [
+    {
+      type: 'taxonomy_term--rental_type',
+      id: '100',
+      attributes: {
+        name: 'Estate'
+      }
+    },
+    {
+      type: 'taxonomy_term--rental_type',
+      id: '101',
+      attributes: {
+        name: 'Terrace House'
+      }
+    },
+    {
+      type: 'taxonomy_term--rental_type',
+      id: '102',
+      attributes: {
+        name: 'Palace'
+      }
+    }
+  ];
 
   this.get('/node/rental', function(db, request) {
     if(request.queryParams["filter[field_city][value]"] !== undefined) {
@@ -58,5 +147,13 @@ export default function() {
   // Find and return the provided rental from our rental list above
   this.get('/node/rental/:id', function (db, request) {
     return { data: rentals.find((rental) => request.params.id === rental.id) };
+  });
+
+  this.get('/file/file/:id', function (db, request) {
+    return { data: files.find((file) => request.params.id === file.id) };
+  });
+
+  this.get('/taxonomy_term/rental_type/:id', function (db, request) {
+    return { data: types.find((rentalType) => request.params.id === rentalType.id) };
   });
 }
